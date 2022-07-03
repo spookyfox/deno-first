2905305430943d055f54ba72de25e9f282272256
spookyfox <spooky.fox@web.de>
2022-07-03 08:06:26 +0200

    first commit

--- deno-api/index.js
+++ deno-api/index.js
@@ -1,6 +1,6 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 import PouchDB from 'https://deno.land/x/pouchdb_deno@v1.0.0-PouchDB+7.2.2/modules/pouchdb/mod.ts'
-
+/*
 // Use the 'idb' afapter for IndexedDB and persistence to disk.
 const db = new PouchDB('mydb', { adapter: 'idb' })
 const   user = 'apikey-4acafc3dea60422083a59256c4f63d92',
@@ -21,7 +21,7 @@ PouchDB.sync( db, remote );
 
 db.get("483ba335-027b-483f-9ad9-12aebc2149f4").then(console.log);
 
-
+*/
 
 serve((_req) => {
   return new Response(
@@ -31,7 +31,7 @@ serve((_req) => {
     </head>
     <body>
       <h1>Example</h1>
-      <button width="100px" onclick="navigator.geolocation.getCurrentPosition((a)=>console.log(a))">
+      <button style={width:100px} onclick="navigator.geolocation.getCurrentPosition((a)=>console.log(a))">
     </body>
   </html>`,
   {
